ade2e6a3b0f38771d94985900b83c1e3cced7dd8
jojev <jo.jevtic999@gmail.com>
2022-04-13 12:49:51 +0200

    added model for users

---
+++ backend/admin/src/main/java/com/bsep/admin/app/model/User.java
@@ -0,0 +1,165 @@
+package com.bsep.admin.app.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Inheritance(strategy= InheritanceType.JOINED)
+@SQLDelete(sql = "UPDATE users SET deleted = true WHERE id=? AND version = ?")
+@Where(clause = "deleted=false")
+public abstract class User implements UserDetails {
+    @Version
+    @Column(name = "version", columnDefinition = "integer DEFAULT 0", nullable = false)
+    private Long version;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    private String lastName;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String emailAddress;
+
+    private String password;
+
+    private byte[] salt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles;
+
+    @JsonIgnore
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    public User(Long version, Integer id, String lastName, String name, String emailAddress, String password, List<Role> roles, byte[] salt) {
+        this.version = version;
+        this.id = id;
+        this.lastName = lastName;
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.roles = roles;
+        this.salt = salt;
+    }
+
+    public User(String lastName, String name, String emailAddress, String password, List<Role> roles, byte[] salt) {
+        this.lastName = lastName;
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.roles = roles;
+        this.salt = salt;
+    }
+
+    public User() {
+
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getUsername() {
+        return emailAddress;
+    }
+}
